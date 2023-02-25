e4349fe9c2b6b7abec951021eb3be3569cbefba0
Hemangi Chauhan <hemangi3317@gmail.com>
2023-02-25 07:38:16 +0530

    update

---
+++ app/Http/Controllers/API/UserController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Auth;
+use Validator;
+
+class UserController extends Controller
+{
+    public function loginUser(Request $request): Response
+    {
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+   
+        if($validator->fails()){
+
+            return Response(['message' => $validator->errors()],401);
+        }
+   
+        if(Auth::attempt($request->all())){
+
+            $user = Auth::user(); 
+    
+            $success =  $user->createToken('MyApp')->plainTextToken; 
+        
+            return Response(['token' => $success],200);
+        }
+
+        return Response(['message' => 'email or password wrong'],401);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function userDetails(): Response
+    {
+        if (Auth::check()) {
+
+            $user = Auth::user();
+
+            return Response(['data' => $user],200);
+        }
+
+        return Response(['data' => 'Unauthorized'],401);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function logout(): Response
+    {
+        $user = Auth::user();
+
+        $user->currentAccessToken()->delete();
+        
+        return Response(['data' => 'User Logout successfully.'],200);
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\UserController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+
+Route::post('login',[UserController::class,'loginUser']);
+
+
+Route::group(['middleware' => 'auth:sanctum'],function(){
+    Route::get('user',[UserController::class,'userDetails']);
+    Route::get('logout',[UserController::class,'logout']);
+});
